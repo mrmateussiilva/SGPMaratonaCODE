415d03425a7dbc7c1b983d86caa2fe541cfd13ac
mrmateussiilva <mateuspython3010@gmail.com>
2025-02-11 23:34:08 -0300

    Melhorando o backedn

--- sgp/app.py
+++ sgp/app.py
@@ -1,35 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,flash,redirect,url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import CSRFProtect
 from form import PainelForm,TipoDeProdutoForm,FichaPedido,TotemForm
+from model import inserDB,get_all_pedidosDB
+
 
 app = Flask(__name__)
 app.secret_key = b'mateus jose da silva'
 bootstrap = Bootstrap5(app)
 csrf = CSRFProtect(app)
 
-
 @app.route("/")
 @app.route("/home")
-def home():
-    return render_template("index.html")
-
-
-data_pedidos = [
-    {"id":'1','descricao':'Painel Redondo BATMAN','value':'90,00','date_envio':'03/11/2024',"forma_envio":'SEDEX'},
-    {"id":'2','descricao':'Painel Redondo BATMAN','value':'90,00','date_envio':'03/11/2024',"forma_envio":'SEDEX'},
-    {"id":'3','descricao':'Painel Redondo BATMAN','value':'90,00','date_envio':'03/11/2024',"forma_envio":'SEDEX'},
-]
-
-@app.route("/pedidos")
-def pedidos():
-    titles = tuple(map(lambda t: t.replace('_'," ").title(),data_pedidos[0].keys()))
-    return render_template("pedidos/index.html",data=data_pedidos,titles=titles)
+def home():    
+    return render_template("index.html",pedidos=get_all_pedidosDB())
 
 
 @app.route("/pedidos/criar",methods=["GET","POST"])
 def pedidos_criar():
-    form_top = FichaPedido(idd='1')
+    idd = len(get_all_pedidosDB())
+    form_top = FichaPedido(idd=idd)
     form_produto = TipoDeProdutoForm()
     form_painel = PainelForm()
     form_totem = TotemForm()
@@ -42,16 +32,21 @@ def pedidos_criar():
             )
     
     if request.method == "POST":
-        # return f"{request.form}"
-        if form_top.validate_on_submit():
-            return f"{form_top}"
+        if inserDB(request.form):
+            flash("Pedido Criado com sucesso","info")
+            return redirect('/home')
         else:
-            return f"{request.form}"
+            flash("Error ao criar pedido","error")
+            return redirect('/home')
+            
+            # return redirect(url_for('pedidos','criar'))
+
+""" 
+
+'idd', '1'), ('name_client', 'Matesu'), ('data_entrada', '2025-01-29'), ('data_envio', '2025-02-05'), ('forma_de_envio', 'pac'), ('observacao', ''), ('csrf_token', 'ImE2YzFjMDRkZmQ3NDhkN2IzMDY1MTllOGMxODgzNGY0NjAyZTExZGQi.Z6vkag.vhc2PExdgItq132lLmNf_biYjoE'), ('btn_save', 'Salvar')
 
 
-
-
-
+ """
 
 
 if __name__ == "__main__":
