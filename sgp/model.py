1bfa189286198277972d726e252c93a0e61b8ce6
mrmateussiilva <mateuspython3010@gmail.com>
2025-02-11 23:33:48 -0300

    Create interface com banco

--- sgp/model.py
+++ sgp/model.py
@@ -1,5 +1,22 @@
-import dataset
+from tinydb import TinyDB
 
 
+db = TinyDB("pedidos.json")
 
-db = dataset.connect('sqlite:///base_dev.db')
+
+def inserDB(data:dict) -> bool:
+    TinyDB.default_table_name = "pedidos"
+    db.insert(data)
+
+
+def get_all_pedidosDB() -> dict:
+    TinyDB.default_table_name = "pedidos"
+    return db.all()
+
+
+if __name__ == "__main__":
+    dados = get_all_pedidosDB() 
+    for dado in dados:
+        print(dado["idd"])
+
+
