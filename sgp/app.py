28908a300fdead8f1ea92f9251fe326d35046d3d
mrmateussiilva <mateuspython3010@gmail.com>
2025-02-20 01:18:58 -0300

    remodelando o app

--- sgp/app.py
+++ sgp/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request,flash,redirect,url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import CSRFProtect
-from form import PainelForm,TipoDeProdutoForm,FichaPedido,TotemForm
-from model import get_all_pedidosDB
+
 
 # Blueprints
 from pedidos.pedidos import bp_pedidos
@@ -13,10 +12,23 @@ app.secret_key = b'mateus jose da silva'
 bootstrap = Bootstrap5(app)
 csrf = CSRFProtect(app)
 
+data = {
+        "idd": "5",
+        "name_client": "Teste do mobile ",
+        "data_entrada": "2025-02-20",
+        "data_envio": "2025-02-28",
+        "forma_de_envio": "entrega_colatina",
+            "observacao": "",
+}
+
+
+
+
+
 @app.route("/")
 @app.route("/home")
 def home():    
-    return render_template("index.html",pedidos=get_all_pedidosDB())
+    return render_template("index.html",pedido=data)
 
 
 
